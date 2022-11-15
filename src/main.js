576cfb8359035b20768fa7598bece3d4b6dd4694
AV-G <68665243+AV-G@users.noreply.github.com>
2022-11-15 21:24:49 +0100

    chunk generation

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import { LoadScene } from "./scenes/LoadScene.js";
 import { StageScene } from "./scenes/StageScene.js";
-
+import { Tile } from "./classes/Tile.js";
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     pixelArt: true,
     physics: {
         default: 'arcade',
+    },
+    scale: {
+        mode: Phaser.Scale.RESIZE, // you can find another types in Phaser.Scale.ScaleModeType: RESIZE | FIT | ENVELOP ...
+        autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     scene: [ LoadScene, StageScene ]
 };
